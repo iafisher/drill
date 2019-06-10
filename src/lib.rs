5e80f60b0632325a920a346b76a259872fd8fc3e
Ian Fisher <iafisher@protonmail.com>
2019-06-10 18:28:36 -0400

    Implement rudimentary quiz-taking

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,41 @@
+use std::io;
+use std::io::Write;
+
 pub trait Question {
-    fn ask(&self);
+    fn ask(&self) -> bool;
+}
+
+pub struct Answer<'a> {
+    pub variants: Vec<&'a str>,
+}
+
+impl<'a> Answer<'a> {
+    pub fn check(&self, guess: &str) -> bool {
+        for variant in self.variants.iter() {
+            if *variant == guess {
+                return true;
+            }
+        }
+        false
+    }
 }
 
 pub struct ShortAnswerQuestion<'a> {
     pub text: &'a str,
-    pub answers: Vec<&'a str>,
+    pub answer: Answer<'a>,
 }
 
 impl<'a> Question for ShortAnswerQuestion<'a> {
-    fn ask(&self) {
-        println!("{}", self.text);
+    fn ask(&self) -> bool {
+        println!("{}\n", self.text);
+
+        print!("> ");
+        io::stdout().flush();
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        self.answer.check(&guess.trim_end())
     }
 }
 
@@ -19,8 +45,23 @@ pub struct Quiz {
 
 impl Quiz {
     pub fn take(&self) {
+        let mut total_correct = 0;
+        let mut total = 0;
         for question in self.questions.iter() {
-            question.ask();
+            let correct = question.ask();
+
+            total += 1;
+            if correct {
+                println!("\nCorrect!");
+                total_correct += 1;
+            } else {
+                println!("\nIncorrect.");
+            }
+        }
+
+        if total > 0 {
+            let score = (total_correct as f64) / (total as f64) * 100.0;
+            println!("\n{} correct out of {} ({}%).", total, total_correct, score);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use popquiz::*;
 
 fn main() {
     let q = ShortAnswerQuestion {
-        text: "What is the capital of Bulgaria?", answers: vec!["Sofia"]
+        text: "What is the capital of Bulgaria?", answer: Answer { variants: vec!["Sofia"] }
     };
     let quiz = Quiz { questions: vec![Box::new(q)] };
     quiz.take();
