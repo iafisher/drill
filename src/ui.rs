582c1e042b82f3ec35ba45ed79cf87b84be7af94
Ian Fisher <iafisher@protonmail.com>
2019-11-19 07:35:46 -0800

    Modify format of score report at end of quiz

--- src/ui.rs
+++ src/ui.rs
@@ -121,7 +121,6 @@ impl CmdUI {
             let score_as_str = format!("{:.1}%", (results.score as f64) / 10.0);
 
             my_print!("\n\n")?;
-            my_print!("Score: ")?;
             my_print!("{}", score_as_str.cyan())?;
             my_print!(" out of ")?;
             my_print!("{}", format!("{}", results.total).cyan())?;
@@ -130,16 +129,15 @@ impl CmdUI {
             } else {
                 my_println!(" questions")?;
             }
-            my_print!("  {}", format!("{}", results.total_correct).green())?;
+            my_print!("{}", format!("{}", results.total_correct).green())?;
             my_print!(" correct\n")?;
             if results.total_partially_correct > 0 {
                 my_print!(
-                    "  {}",
-                    format!("{}", results.total_partially_correct).bright_green()
+                    "{}", format!("{}", results.total_partially_correct).bright_green()
                 )?;
                 my_print!(" partially correct\n")?;
             }
-            my_print!("  {}", format!("{}", results.total_incorrect).red())?;
+            my_print!("{}", format!("{}", results.total_incorrect).red())?;
             my_print!(" incorrect\n")?;
         }
         Ok(())
